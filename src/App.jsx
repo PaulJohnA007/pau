5bcb1ca2898c86c539dfcc0040dd1d95d46f1601
Paul John Acabo <acabopaul@gmail.com>
2025-03-03 17:46:32 +0800

    Change UI Design,Fonts,AddHero Picture

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AboutMe from './components/About/AboutMe';
 import Home from './components/Home';
 
 
-
- 
- 
-function App() {
-  
+const App = () => {
   return (
-    <div>
-      <Home />  
-      <AboutMe />
-    </div>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutMe />} />
+        {/* Add other routes here */}
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,34 @@
-import React from 'react';
- 
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <section id="hero">
+      <div className="background">
+        <div className="animated-bg"></div>
+      </div>
       <div className="container">
         {/* Navbar */}
         <div className="navbar">
           <div className="logo">
             <a href="#">PJx.</a>
           </div>
-          <div className="menu">
-            <a href="#">Home</a>
-            <a href="#">About</a>
-            <a href="#">Contact</a>
-            <a href="#">Projects</a>
+          <div className={`menu ${menuOpen ? 'open' : ''}`}>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/projects">Projects</Link>
+          </div>
+          <div className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
           </div>
         </div>
- 
-        <div className="hero-content"> 
+
+        <div className="hero-content">
           <div className="hero-box">
             <h1 className="hero-heading">Paul John Acabo</h1>
             <h2 className="hero-2ndheading">I'm a Developer</h2>
@@ -34,11 +44,10 @@ const Home = () => {
               <a href="#"><i className="bx bxl-instagram"></i></a>
               <a href="#"><i className="bx bxl-linkedin-square"></i></a>
             </div>
-            
-         
           </div>
- 
-          
+          <div className="hero-image">
+            <img src="src/assets/1b1.jpg" alt="Profile" />
+          </div>
         </div>
       </div>
     </section>
